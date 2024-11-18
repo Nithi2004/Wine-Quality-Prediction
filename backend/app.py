240fa5823616cbc5009eb4dc3bf8bde2af74b383
Nithi2004 <nnithish567@gmail.com>
2024-11-18 13:42:35 +0530

    modified

--- backend/app.py
+++ backend/app.py
@@ -22,5 +22,5 @@ def predict():
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    # Use host 0.0.0.0 to make it accessible on Render
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
